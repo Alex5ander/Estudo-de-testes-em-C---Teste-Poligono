bb5e67ca48edd153ba6438b871a01bbe3b391db4
Alexsander <alexsandergutierreez@gmail.com>
2022-04-14 22:07:22 -0300

    Aula

--- ClassLibrary1/TestePoligono.cs
+++ ClassLibrary1/TestePoligono.cs
@@ -40,5 +40,20 @@ namespace ClassLibrary1
             Assert.AreEqual(5000, area, "Falha teste area do triângulo");
             Assert.AreEqual("equilatero", tipo, "Falha teste tipo do triângulo");
         }
+
+        [TestCase(100, 100, 100, 300, "equilatero")]
+        [TestCase(100, 200, 300, 600, "escaleno")]
+        [TestCase(100, 200, 100, 400, "isoceles")]
+        public void TestarTriangulo(double L1, double L2, double L3, double perimetroEsperado, string tipoEsperado)
+        {
+            Triangulo obj = new Triangulo(L1, L2, L3);
+            double perimetro = obj.Perimetro();
+            double area = obj.Area(100);
+            string tipo = obj.Tipo();
+
+            Assert.AreEqual(perimetroEsperado, perimetro, "Falha teste perimetro do trângulo "+tipoEsperado);
+            Assert.AreEqual(5000, area, "Falha teste area do triângulo "+tipoEsperado);
+            Assert.AreEqual(tipoEsperado, tipo, "Falha teste tipo do triângulo "+tipoEsperado);
+        }
     }
 }
